dc9054b1909c3883f91be2d86ba3d9bd8c374a9d
numan <numan.h.99@gmail.com>
2021-03-29 22:58:51 +0800

    merged schemas into one

--- models/hotels.js
+++ models/hotels.js
@@ -2,11 +2,26 @@ const mongoose = require('mongoose');
 const hotelSchema = new mongoose.Schema({
   name: String,
   location: String,
-  vacancies: Boolean,
-  rooms: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Room'
-  }]
+  img: String,
+  rooms: [
+    {
+      size: String,
+      price: Number,
+      roomNumber: Number,
+      dates: [ 
+        {
+          date: Date,
+          slot: {
+            booked: Boolean,
+            user: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: 'User'
+            }
+          }
+        }
+      ]
+    }
+  ]
 });
                                   
 const Hotels = mongoose.model('Hotel', hotelSchema)
