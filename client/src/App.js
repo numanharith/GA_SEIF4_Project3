366578665b2d60bc0062dbe09002b375892f9c99
cynthiajteo <cynthiajteo@gmail.com>
2021-03-31 18:42:32 +0800

    added key to remove key error

--- client/src/App.js
+++ client/src/App.js
@@ -7,60 +7,68 @@ import Nav from './components/Nav';
 const axios = require('axios');
 
 export class App extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			hotels: [],
-		};
-	}
+        this.state = {
+            hotels: [],
+        };
+    }
 
-	fetchdata = async () => {
-		try {
-			const response = await axios.get('/hotels');
-			console.log(response.data);
-			this.setState({
-				hotels: response.data,
-			});
-		} catch (err) {
-			console.log(err);
-		}
-	};
+    fetchdata = async () => {
+        try {
+            const response = await axios.get('/hotels');
+            //console.log(response.data);
+            this.setState({
+                hotels: response.data,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-	componentDidMount = () => {
-		this.fetchdata();
-	};
+    componentDidMount = () => {
+        this.fetchdata();
+    };
 
-	render() {
-		return (
-			<React.Fragment>
-        <Nav />
-        <div className="hotelCards album py-5 bg-light">
-          <div className="container">
-            <div className="row">          
-              {this.state.hotels.map((hotel, index) => {
-                return (
-                  <Hotel hotel={hotel} />
-                )
-              })}
-            </div>
-          </div>
-        </div>
-        <BookRoom />
-        <div className="roomCards album py-5 bg-light">
-          <div className="container">
-            <div className="row">          
-              {this.state.hotels.map((hotel, index) => {
-                return (
-                  <HotelRoomType hotel={hotel} />
-                )
-              })}
-            </div>
-          </div>
-        </div>
-			</React.Fragment>
-		);
-	}
+    render() {
+        return (
+            <React.Fragment>
+                <Nav />
+                <div className='hotelCards album py-5 bg-light'>
+                    <div className='container'>
+                        <div className='row'>
+                            {this.state.hotels.map((hotel, index) => {
+                                return (
+                                    <Hotel
+                                        hotel={hotel}
+                                        key={hotel._id}
+                                        fetchdata={this.fetchdata}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+                <BookRoom />
+                <div className='roomCards album py-5 bg-light'>
+                    <div className='container'>
+                        <div className='row'>
+                            {this.state.hotels.map((hotel, index) => {
+                                return (
+                                    <HotelRoomType
+                                        hotel={hotel}
+                                        key={hotel._id}
+                                        fetchdata={this.fetchdata}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
